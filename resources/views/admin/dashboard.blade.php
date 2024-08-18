28b3bd2e0ee77339edb0946bc51d422806daea56
Mudzakki Ahmad <zihad15april@gmail.com>
2024-08-18 13:39:17 +0700

    repair chart

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
     <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item active">Dashboard</li>
     </ol>
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-xl-12">
             <div class="card mb-4">
                 <div class="card-header">
@@ -14,6 +14,17 @@
                     Failed login attempts in the past 30 days
                 </div>
                 <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+            </div>
+        </div>
+    </div> -->
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-chart-bar me-1"></i>
+                    Failed login attempts in the past 30 days
+                </div>
+                <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
             </div>
         </div>
     </div>
@@ -93,6 +104,55 @@
             },
             gridLines: {
             color: "rgba(0, 0, 0, .125)",
+            }
+        }],
+        },
+        legend: {
+        display: false
+        }
+    }
+    });
+</script>
+
+<script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#292b2c';
+
+    // Bar Chart Example
+    var ctx = document.getElementById("myBarChart");
+    var myLineChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+        label: "Failed Login Attempts",
+        backgroundColor: "rgba(2,117,216,1)",
+        borderColor: "rgba(2,117,216,1)",
+        data: loginAttemptData,
+        }],
+    },
+    options: {
+        scales: {
+        xAxes: [{
+            time: {
+            unit: 'month'
+            },
+            gridLines: {
+            display: false
+            },
+            ticks: {
+            maxTicksLimit: 6
+            }
+        }],
+        yAxes: [{
+            ticks: {
+            min: 0,
+            max: maxLimitData,
+            maxTicksLimit: 5
+            },
+            gridLines: {
+            display: true
             }
         }],
         },
